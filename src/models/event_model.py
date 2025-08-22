7426c82dc765cb6b86dce60e5d97d1fa554a4009
nia3zzz <mushfiqurniaz33@gmail.com>
2025-08-23 03:00:00 +0600

    add db models & update folder structure

---
+++ src/models/event_model.py
@@ -0,0 +1,46 @@
+from ..db.engine import base
+from uuid import uuid4
+from sqlalchemy import UUID, ForeignKey, String, DateTime, JSON, Boolean, func
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+import datetime
+from .user_model import User
+
+
+# defining the event model
+class Event(base):
+    __tablename__ = "events"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, insert_default=uuid4, index=True)
+
+    user_id: Mapped[UUID] = mapped_column(
+        ForeignKey("users.id"), nullable=False, index=True
+    )
+
+    description: Mapped[str] = mapped_column(String, nullable=False)
+
+    roles: Mapped[list[dict]] = mapped_column(JSON, nullable=False, default=list)
+
+    is_recurring: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+
+    recurrence_rule: Mapped[str] = mapped_column(String, nullable=False, default=None)
+
+    start_timestamp: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False)
+
+    end_timestamp: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=False)
+
+    recurrence_end_timestamp: Mapped[datetime.datetime] = mapped_column(
+        DateTime, nullable=True, default=None
+    )
+
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), onupdate=func.now()
+    )
+
+    user: Mapped["User"] = relationship("User", back_populates="events")
+
+    def __repr__(self) -> str:
+        return f"Event(id={self.id!r}, user_id={self.user_id!r})"
---
+++ src/models/user_model.py
@@ -0,0 +1,38 @@
+from ..db.engine import base
+from sqlalchemy import String, DateTime, func, UUID, String
+from uuid import uuid4
+from sqlalchemy.orm import Mapped, relationship, mapped_column
+import datetime
+from .event_model import Event
+
+
+# defining the user model
+class User(base):
+    __tablename__ = "users"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, insert_default=uuid4, index=True)
+
+    first_name: Mapped[str] = mapped_column(String(30))
+
+    last_name: Mapped[str] = mapped_column(String(30), nullable=False)
+
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+
+    profile_picture: Mapped[str] = mapped_column(
+        String(500),
+        insert_default="https://www.shutterstock.com/image-vector/default-avatar-profile-icon-social-600nw-1677509740.jpg",
+        nullable=False,
+    )
+
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), onupdate=func.now()
+    )
+
+    events: Mapped[list["Event"]] = relationship(back_populates="user")
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, email={self.email!r})"
