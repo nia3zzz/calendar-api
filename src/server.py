a4735775a7544d3b28e7423e777b89fa21da372c
nia3zzz <mushfiqurniaz33@gmail.com>
2025-08-26 20:34:35 +0600

    add jwt decode authentication middleware

--- src/server.py
+++ src/server.py
@@ -3,12 +3,50 @@ from dotenv import load_dotenv
 # load the .env variables into runtime environment
 load_dotenv()
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
+from fastapi.responses import JSONResponse
 from db.engine import get_db
 from sqlalchemy.orm import Session
 from routes.auth_routes import auth_routes
+import jwt
+from lib.assign_jwt import secret_key
 
 v1 = FastAPI()
+
+
+# define a auth middleware for every route except signup, login and the healthcheck
+@v1.middleware("http")
+async def AuthenticationMiddleware(request: Request, call_next):
+    # check if its the allowed routes
+    if request.url.path in ["/api/v1/auth/signup", "/api/v1/auth/login", "/"]:
+        return await call_next(request)
+
+    # check for the cookie
+    hold_cookie = request.cookies.get("Access_Cookie")
+
+    if hold_cookie == None:
+        return JSONResponse(
+            status_code=401,
+            content={"staus": "error", "message": "Unauthorized, login first."},
+        )
+
+    # decode the jwt from the cookie and attach the user id in request object
+    try:
+        payload = jwt.decode(hold_cookie, str(secret_key), algorithms=["HS256"])
+
+        request.state.user_id = payload.get("user_id")
+        return await call_next(request)
+    except (jwt.InvalidTokenError, jwt.ExpiredSignatureError):
+        return JSONResponse(
+            status_code=401,
+            content={"staus": "error", "message": "Unauthorized, login first."},
+        )
+    except:
+        return JSONResponse(
+            status_code=500,
+            content={"status": "error", "message": "Something went wrong."},
+        )
+
 
 # include auth routes
 v1.include_router(router=auth_routes, prefix="/api/v1/auth")
